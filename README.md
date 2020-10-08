e2d28147dd3aeaf4ed1f7a39ed395b20aa890e11
Nate Nasteff <nnasteff@protonmail.com>
2020-10-08 03:04:56 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+
+
+#    <center> The Jazz Lounge v2
+
+
+// Project started 10/7/2020 //
+
+I wrote this site from scratch originally using pure PHP, HTML5 and CSS. I
+decided it was time to update it a bit (mostly the front end), and as a way
+to force myself to learn something new, decided to use the Symfony framework
+this time around.
+
+Symfony seems extremely popular nowadays, and I love the fact that they are
+completely open source. Symfony also seems to have a very active community,
+with no shortage of information on how to get the job done.
+
+You can find the Symfony repo hosted right here on Github ([link](https://github.com/symfony/symfony)).
+
---
+++ src/Controller/Home.php
@@ -0,0 +1,41 @@
+<?php
+// src/Controller/Home.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+include("header.html");
+
+class Home
+{
+
+/**  
+*  @Route("/index")
+*/
+    public function number(): Response
+    {
+        return new Response(
+            '<h3 class="display-3 text-center">
+            Welcome to the Jazz Emporium!
+          </h3>
+          <table class="center">
+            <tr>
+              <td>
+                <p class="lead">
+          
+                Here you will find some of the best Jazz albums of all time for 
+                sale. These are not CDs, digital downloads or any other type of 
+                digital media, they are vinyl records. They are reprints of 
+                original albums released years ago that, in some cases, were 
+                out of print for decades. Now, amongst the rising resurgance of 
+                popularity in vinyl albums, they are being reproduced for all to 
+                enjoy.<br>Check out our catalog!
+                
+                </p>
+              </td>
+            </tr>
+          </table>'
+        );
+    }
+}
