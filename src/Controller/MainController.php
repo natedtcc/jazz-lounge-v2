bda80ab5d23f04377941b4bee8a0cab86ee0d25e
Nate Nasteff <nnasteff@protonmail.com>
2020-10-10 12:54:40 -0400

    basic templating for browse page

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,14 +3,12 @@
 
 namespace App\Controller;
 
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Request;
-use App\Entity\Products;
 
 class MainController extends AbstractController
 {
@@ -29,7 +27,7 @@ class MainController extends AbstractController
   # TODO: Implement pagination
     public function browse(EntityManagerInterface $em, PaginatorInterface $paginator, Request $request)
     {
-      $sql_str = "SELECT products.artist FROM App\Entity\Products products";
+      $sql_str = "SELECT a FROM App\Entity\Products a";
       $query = $em->createQuery($sql_str);
 
       $pagination = $paginator->paginate($query, $request->query->getInt('page', 4), 10);
