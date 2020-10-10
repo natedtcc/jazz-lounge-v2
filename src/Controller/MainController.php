f63bff521d32f4bab36c72b4f8f83996d2486666
Nate Nasteff <nnasteff@protonmail.com>
2020-10-10 11:36:28 -0400

    added Entity/Repo products table, migrated to mysql

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -10,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Products;
 
 class MainController extends AbstractController
 {
@@ -28,10 +29,10 @@ class MainController extends AbstractController
   # TODO: Implement pagination
     public function browse(EntityManagerInterface $em, PaginatorInterface $paginator, Request $request)
     {
-      $sql_str = "SELECT artist FROM products";
+      $sql_str = "SELECT products.artist FROM App\Entity\Products products";
       $query = $em->createQuery($sql_str);
 
-      $pagination = $paginator->paginate($query, $request->query->getInt('page', 1), 10);
+      $pagination = $paginator->paginate($query, $request->query->getInt('page', 4), 10);
       return $this->render('browse/browse.html.twig', ['pagination' => $pagination]);
     }
 
---
+++ src/Entity/Products.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProductsRepository::class)
+ */
+class Products
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $product_id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $artist;
+
+    /**
+     * @ORM\Column(type="decimal", precision=4, scale=2)
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getProductId(): ?int
+    {
+        return $this->product_id;
+    }
+
+    public function setProductId(int $product_id): self
+    {
+        $this->product_id = $product_id;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getArtist(): ?string
+    {
+        return $this->artist;
+    }
+
+    public function setArtist(string $artist): self
+    {
+        $this->artist = $artist;
+
+        return $this;
+    }
+
+    public function getPrice(): ?string
+    {
+        return $this->price;
+    }
+
+    public function setPrice(string $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
