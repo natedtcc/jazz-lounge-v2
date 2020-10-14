b0796a3d97dd7bce0a1565e1d9ac6e9b0edfd805
Nate Nasteff <nnasteff@protonmail.com>
2020-10-14 15:07:42 -0400

    cart template page started, more shopping cart logic and auth checks implemented

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -41,14 +41,34 @@ class MainController extends AbstractController
 /**  
 *@Route("/add_cart", name="add_cart")
 */
-  public function cart(Request $request)
+  public function cart(Request $request, EntityManagerInterface $em)
   {
+    /* If the user is not logged in, redirect them to login */
+
+    if ($this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY')){
+      return $this->redirect('login');
+    }
+    
+    $sql_str = "SELECT a FROM App\Entity\Products a";
+    $query = $em->createQuery($sql_str);
+    
+    
     /* Get product ID from request and add them
     to the cart array within the user session */
     
+
     $cart = $request->query->getInt('pid');
     $val = $this->get('session')->get('cart');
-    $val[] = $cart;
+
+    /* If the item added to the cart is already in the cart,
+    increment the quantity. Otherwise, quantity is set to 1 */
+    
+    if (isset($val[$cart]))
+      $val[$cart]['quantity']++;
+    
+      else 
+      $val[$cart] = ['pid' => 1, 'price' => 3.50,'quantity' => 1];
+    
     $this->get('session')->set('cart', $val);
     return $this->redirect('browse');
 
@@ -59,7 +79,13 @@ class MainController extends AbstractController
 */
   public function view_cart(Request $request)
   {
+    /* If the user is not logged in, redirect them to login */
+    if ($this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY')){
+      return $this->redirect('login');
+    }
+
     $cart = $this->get('session')->get('cart');
+    print_r($cart);
     // $value = $session[1];
     return $this->render('cart/cart.html.twig', ['cart' => $cart]);
   }
