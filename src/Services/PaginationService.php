bb793732e833cdc8b81e8881dbb68c585d5a96c6
Nate Nasteff <nnasteff@protonmail.com>
2020-10-16 21:53:29 -0400

    added phpunit

--- src/Services/PaginationService.php
+++ src/Services/PaginationService.php
@@ -6,48 +6,52 @@ namespace App\Services;
 use Knp\Component\Pager\PaginatorInterface;
 use Doctrine\ORM\EntityManagerInterface;
 
-    /**
-   * PaginatorInterface represents a paginated request
-   * to the database.
-   * 
-   * Method:
-   * 
-   * paginate()
-   * 
-   * @return Pagination
-   */
+/**
+ * PaginatorInterface represents a paginated request
+ * to the database.
+ * 
+ * Method:
+ * 
+ * paginate()
+ * 
+ * @return Pagination
+ */
 
-class PaginationService{
+class PaginationService
+{
 
   private $entity;
   private $paginator;
 
   public function __construct(
-    EntityManagerInterface $entity, PaginatorInterface $paginator){
-        $this->entity = $entity;
-        $this->paginator = $paginator;
-      }
-    /**
+    EntityManagerInterface $entity,
+    PaginatorInterface $paginator
+  ) {
+    $this->entity = $entity;
+    $this->paginator = $paginator;
+  }
+  /**
    * Returns paginated results from a database
    *
    * @param Request $request
    * @return Pagination $pagination
    */
 
-  public function paginate($request){
+  public function paginate($request)
+  {
     /* Create product query for pagination */
-    
+
     $sql_str = "SELECT a FROM App\Entity\Products a";
     $query = $this->entity->createQuery($sql_str);
-    
+
     /* Create paginated results, then return them */
 
     $pagination = $this->paginator->paginate(
-      $query, $request->query->getInt('page', 1), 10
+      $query,
+      $request->query->getInt('page', 1),
+      10
     );
 
     return $pagination;
   }
-
-
 }
